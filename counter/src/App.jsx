c61703d433a6e72be50d5d29510aa7433a07d5d3
Gaalav Gupta <74466835+Gaalav@users.noreply.github.com>
2025-08-24 12:13:36 +0530

    #

---
+++ counter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '1rem' }}>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  )
+}
+
+export default App
